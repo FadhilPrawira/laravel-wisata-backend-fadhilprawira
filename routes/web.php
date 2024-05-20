0ad4ad88938cb23fae08c50af46dc1243ddbaaa1
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-05-21 01:04:54 +0700

    feat: create feature add user, edit user, delete user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController; // Import the UserController class
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +12,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/home', function () {
         return view('pages.dashboard');
     })->name('home');
+    Route::resource('users', UserController::class);
 });
